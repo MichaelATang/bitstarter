44883307d987ad6e69e206da3afec395ab3f6c59
Ubuntu <ubuntu@ip-172-31-23-176.us-west-2.compute.internal>
2013-07-04 22:59:38 +0000

    Web.js uses index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs =  require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html', "utf8");
+
+var output = Buffer(content).toString(); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
